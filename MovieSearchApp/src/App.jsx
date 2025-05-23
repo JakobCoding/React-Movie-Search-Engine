e418d8280fed47b4d24e33a97309f7ce2f339117
Jay Cee <jakechalmers86@gmail.com>
2025-05-23 11:08:44 +0800

    Created MovieCard component, Exported componted and added it to App.jsx

--- MovieSearchApp/src/App.jsx
+++ MovieSearchApp/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// imported MovieCard Component that we created 
+import MovieCard from "./components/MovieCard"
+
+
+// In React a function is also known as a Component
+// A Component is any function in JavaScript that returns some kind of JSX code
+// JSX stand for JavaScript XML it allows Developers to write HTML-like code within JavaScript
+
+// Each component can only contain one parent element ex: only 1 div per component 
+// First Component - Components always start with a capitol letter 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
+    // Inserting a fragment "<> </>"" allows for mulitple div elements within a component 
+    // Now to display our imported MovieCard component that we created 
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <MovieCard movie={{title: "Jake's Movie", release_date: "2025",}}/>
+
     </>
   )
 }
 
-export default App
+export default App;
