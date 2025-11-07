0641a3e1744c19bc5f8eabe42116d4e4bb89ec63
Jake Chalmers <jakechalmers86@gmail.com>
2025-11-07 16:47:43 +0800

    fix: Deleted project to start from scratch

--- MovieSearchApp/src/pages/Favorites.jsx
+++
@@ -1,8 +0,0 @@
-function Favorite() {
-    return <div className="favorites-empty">
-        <h2>No Favorite Movies Yet</h2>
-        <p>Start adding movies to your favorites and they will appear hear!</p>
-    </div>
-}
-
-export default Favorite
