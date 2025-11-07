1558b21773289d8cf7adfb40008dc3966ac62cdd
Jake Chalmers <jakechalmers86@gmail.com>
2025-11-07 18:29:57 +0800

    feat: Import moviecard, add moviecard components to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import "./App.css";
+import MovieCard from "./components/moviecard"
 
 // Component 
 function App() {
-  return <div>simple Component</div>;
+  return (
+    <>
+    <MovieCard movie={{title: "Jake's Film", release_date: "2025"}}/>
+    <MovieCard movie={{title: "Joes's Film", release_date: "2024"}}/>
+    <MovieCard movie={{title: "James's Film", release_date: "2021"}}/>
+    <MovieCard movie={{title: "John's Film", release_date: "2020"}}/>
+    </>
+  );
 }
 
 export default App;
