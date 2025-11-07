00c19a29ed558153fc1bda5558e7103eddf511e9
Jake Chalmers <jakechalmers86@gmail.com>
2025-11-07 18:40:28 +0800

    feat: Add pages folder inside src and create home.jsx inside pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
-import MovieCard from "./components/moviecard"
+
 
 // Component 
 function App() {
   return (
     <>
-    <MovieCard movie={{title: "Jake's Film", release_date: "2025"}}/>
-    <MovieCard movie={{title: "Joes's Film", release_date: "2024"}}/>
-    <MovieCard movie={{title: "James's Film", release_date: "2021"}}/>
-    <MovieCard movie={{title: "John's Film", release_date: "2020"}}/>
+    
+  
+
     </>
   );
 }
